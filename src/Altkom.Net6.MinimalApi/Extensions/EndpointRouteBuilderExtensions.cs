1fdb47a1934f9cabb7f176df85969ca4ff6e51ed
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-10-17 14:16:26 +0200

    Dodanie operacji CRUD

---
+++ src/Altkom.Net6.MinimalApi/Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Altkom.Net6.MinimalApi.Extensions
+{
+    public static class EndpointRouteBuilderExtensions
+    {
+        private static readonly string[] HeadVerbs = new[] { "HEAD" };
+        private static readonly string[] PatchVerbs = new[] { "PATCH" };
+
+        public static IEndpointConventionBuilder MapHead(this IEndpointRouteBuilder endpoints,
+            string pattern,
+            Delegate requestDelegate)
+        {
+            return endpoints.MapMethods(pattern, HeadVerbs, requestDelegate);
+        }
+
+
+        public static IEndpointConventionBuilder MapPatch(this IEndpointRouteBuilder endpoints,
+            string pattern,
+            Delegate requestDelegate)
+        {
+            return endpoints.MapMethods(pattern, PatchVerbs, requestDelegate);
+        }
+    }
+}
--- src/Altkom.Net6.MinimalApi/Program.cs
+++ src/Altkom.Net6.MinimalApi/Program.cs
@@ -1,6 +1,9 @@
 using Altkom.Net6.Domain;
 using Altkom.Net6.Infrastructure;
 using Altkom.Net6.MinimalApi;
+using Altkom.Net6.MinimalApi.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 // var app = WebApplication.Create();
@@ -10,7 +13,7 @@ var builder = WebApplication.CreateBuilder();
 builder.Services.AddSingleton<ICustomerRepository, InMemoryCustomerRepository>(); // Rejestrowanie w konterze wstrzykiwania zale¿noœci
 
 var app = builder.Build();
-    
+
 
 var lambda = () => "Hello from lambda variable";
 string LocalFunction() => "Hello from local function";
@@ -19,18 +22,98 @@ app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/api/customers", (ICustomerRepository repository) => repository.Get());
 
-app.MapGet("/api/customers/{id:int}", (ICustomerRepository repository, int id) =>
-{
-    var customer = repository.Get(id);
+// Przyk³ad z u¿yciem funkcji
+//app.MapGet("/api/customers/{id:int}", (ICustomerRepository repository, int id) =>
+//{
+//    var customer = repository.Get(id);
+
+//    if (customer == null)
+//    {
+//        return Results.NotFound();
+//    }
+//    else
+//    {        
+//        return Results.Ok(customer);
+//    }
+//});
+
+// Przyk³ad z u¿yciem operatora is i ?
+//app.MapGet("/api/customers/{id:int}", (ICustomerRepository repository, int id) 
+//    => repository.Get(id) is Customer customer ? Results.Ok(customer) : Results.NotFound());
+
+// Przyk³ad z u¿yciem operatora switch i Match Patterns
+app.MapGet("/api/customers/{id:int}", (ICustomerRepository repository, int id) => repository.Get(id) switch
+ {
+     Customer customer => Results.Ok(customer),
+     null => Results.NotFound() 
+ }).WithName("GetCustomerById");
+
+
+app.MapGet("/api/customers/{email}", (ICustomerRepository repository, string email) => {
+    var customer = repository.GetByEmail(email);
 
     if (customer == null)
-    {
         return Results.NotFound();
-    }
     else
-    {        
         return Results.Ok(customer);
-    }
+});
+
+app.MapPost("/api/customers", (Customer customer, ICustomerRepository repository) =>
+{
+    repository.Add(customer);
+
+    // z³a praktyka
+    // return Results.Created($"http://localhost:5000/api/customers/{customer.Id}", customer);
+
+    // Dobra praktyka
+    return Results.CreatedAtRoute("GetCustomerById", new { customer.Id }, customer);
+});
+
+app.MapGet("/api/customers/{id}/link", (int id, LinkGenerator linkGenerator) 
+    => $"The link to customer {linkGenerator.GetPathByName("GetCustomerById", values: new { id })}");
+
+app.MapPut("/api/customers/{id}", (int id, Customer customer, ICustomerRepository repository) =>
+{
+    if (id != customer.Id)
+        return Results.BadRequest();
+
+    repository.Update(customer);
+
+    return Results.NoContent();
+
+
+});
+
+app.MapDelete("/api/customers/{id}", (int id, ICustomerRepository repository) => repository.Remove(id));
+
+// HEAD /api/customers/{id}
+//app.MapMethods("/api/customers/{id}", new[] { "HEAD" }, (int id, ICustomerRepository repository) =>
+//{
+//    if (repository.Exists(id))
+//        return Results.Ok();
+//    else
+//        return Results.NotFound();
+//});
+
+// Rekaktoring - u¿ycie metody rozszerzaj¹cej w celu utworzenia metody MapHead
+app.MapHead("/api/customers/{id}", (int id, ICustomerRepository repository) =>
+{
+    if (repository.Exists(id))
+        return Results.Ok();
+    else
+        return Results.NotFound();
+});
+
+// JSON Patch (RFC 6902)
+// https://jsonpatch.com/
+// Content-Type: application/json-patch+json
+
+// dotnet add package Microsoft.AspNetCore.JsonPatch --version 6.0.10
+app.MapPatch("/api/customers/{id}", (int id, 
+    JsonPatchDocument<Customer> patchCustomer, 
+    ICustomerRepository repository) =>
+{
+
 });
 
 app.MapGet("/lambda", lambda);
