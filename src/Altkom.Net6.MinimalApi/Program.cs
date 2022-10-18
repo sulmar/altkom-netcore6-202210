1e35ce7e399f74033f7f8e7b4b0fadb072f5453c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-10-18 10:58:58 +0200

    Odczytywane konfiguracji

--- src/Altkom.Net6.MinimalApi/Program.cs
+++ src/Altkom.Net6.MinimalApi/Program.cs
@@ -12,6 +12,26 @@ var builder = WebApplication.CreateBuilder();
 
 builder.Services.RegisterServices();
 
+// Domyœlnie
+// builder.Configuration.AddJsonFile("appsettings.json", optional: false);
+// builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true);
+// builder.Configuration.AddEnvironmentVariables();
+// builder.Configuration.AddCommandLine(args);
+
+// > SETX ASPNETCORE_ENVIRONMENT Testing
+
+builder.Configuration.AddXmlFile("appsettings.xml", optional: true);
+builder.Configuration.AddIniFile("appsettings.ini", optional: true);
+
+string myKey = builder.Configuration["MyKey"];
+Console.WriteLine(myKey);
+
+string parameter1 = builder.Configuration["Sekcja1:Parametr1"];
+int subparameter1 = int.Parse(builder.Configuration["Sekcja1:Parametr2:Podparametr1"]);
+
+Console.WriteLine(parameter1);
+Console.WriteLine(subparameter1);
+
 var app = builder.Build();
 
 app.MapBasicEndpoints();
@@ -21,5 +41,6 @@ app.MapFilesEndpoints();
 app.MapHtmlEndpoints();
 
 app.UseStaticFiles();
+
 
 app.Run("http://localhost:5000");
